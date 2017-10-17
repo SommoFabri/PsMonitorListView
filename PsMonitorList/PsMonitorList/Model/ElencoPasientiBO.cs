a001fe175a95ba1abfc64d57b27f43f427ee08e8
AlRivoli <italia92>
2017-10-17 14:42:47 +0200

    creata classe ElencoPasientiBO

---
+++ PsMonitorList/PsMonitorList/Model/ElencoPasientiBO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsMonitorList.Model
+{
+    class ElencoPasientiBO
+    {
+        private List<RecordBean> lista = new List<RecordBean>();
+        public static List<RecordBean> getListaAssistitiDaVisualizzare()
+        {
+            List<RecordBean> listaAssistitiDaVisualizzare = new List<RecordBean>();
+
+
+            return listaAssistitiDaVisualizzare;
+            ;
+
+        }
+    }
+
+
+
+}
